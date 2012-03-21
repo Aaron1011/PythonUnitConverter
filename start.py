b3ca70ab9127dabb328f06c6437cd7573090d127
Aaron Hill <aa1ronham@aol.com>
2012-03-21 07:39:25 -0400

    Added base selection functionality for start.py

---
+++ start.py
@@ -0,0 +1,9 @@
+print 'Welcome to Python Unit converter!'
+def __init__():
+    c = raw_input('Choose the unit you would to convert: [I]nches, [F]eet, [Y]ards, [M]iles ')
+    if c == 'i' or c == 'I':
+        print 'You selected inches.'
+    else:
+        print 'Goodbye!'
+if __name__ == '__main__':
+    __init__()
