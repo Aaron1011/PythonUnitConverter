d5e15d02410f6dd00acc936d78b72c7bd8aac2bc
Aaron Hill <aa1ronham@aol.com>
2012-03-24 17:16:36 -0400

    Added feet to inches conversion capability

---
+++ feet.py
@@ -0,0 +1,11 @@
+def feet_inches(n):
+    a = n * 12
+    if a == 1:
+        print str(n) + ' feet = ' + str(a) + ' inch.'
+    else:
+        print str(n) + ' feet = ' + str(a) + ' inches.'
+def foot():
+    a = input('How many feet would you like to convert? ')
+    b = raw_input('What you you like to convert to? [I]nches, [Y]ards, [M]iles: ')
+    if b == 'I' or b == 'i':
+        feet_inches(a)
--- start.py
+++ start.py
@@ -1,10 +1,13 @@
 from inches import *
+from feet import *
 print 'Welcome to Python Unit converter!'
 
 def __init__():
     c = raw_input('Choose the unit you would to convert: [I]nches, [F]eet, [Y]ards, [M]iles ')
     if c == 'i' or c == 'I':
         inch()
+    elif c == 'F' or c == 'f':
+        foot() 
     else:
         print 'Goodbye!'
 if __name__ == '__main__':
