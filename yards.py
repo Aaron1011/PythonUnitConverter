8df61e9e164b07e704dc500fb857df78a76730b0
Aaron Hill <techsavvy8989@gmail.com>
2012-03-25 17:20:59 -0400

    Added yards to feet conversion capability

--- yards.py
+++ yards.py
@@ -6,6 +6,12 @@ def yards_inches(n):
     else:
         print str(n) + ' yards = ' + str(a) + ' inch(es).'
 
+def yards_feet(n):
+    a = n * 3
+    if n == 1
+        print str(n) + ' yard = ' + str(a) + ' feet.'
+    else:
+        print str(n) + ' yards = ' + str(a) + ' feet.'
 def yard():
     try:
         a = float(raw_input('How many yards would you like to convert? '))
@@ -15,5 +21,7 @@ def yard():
     b = raw_input('What would you like to convert to? [I]inches, [F]eet, [M]iles: ')
     if b == 'I' or b == 'i':
         yards_inches(a)
+    elif b == 'F' or b == 'f':
+        yards_feet(a)
     else:
         print 'That conversion is not available yet.'
