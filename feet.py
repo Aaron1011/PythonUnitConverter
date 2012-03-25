5c73fc9df2b4176dcf8bd6b0744939d174193553
Aaron Hill <techsavvy8989@gmail.com>
2012-03-25 08:39:05 -0400

    Added capabilities to check to number to convert is a number, and added program termination at the end of conversion

--- feet.py
+++ feet.py
@@ -1,12 +1,18 @@
 def feet_inches(n):
-    a = n * 12
-    if a == 1:
-        print str(n) + ' feet = ' + str(a) + ' inch.'
+    b = int(n) * 12
+    if  int(n) == 1:
+        print str(n) + ' feet = ' + str(b) + ' inch.'
+        quit()
     else:
-        print str(n) + ' feet = ' + str(a) + ' inches.'
+        print str(n) + ' feet = ' + str(b) + ' inches.'
+        quit()
 
 def foot():
-    a = input('How many feet would you like to convert? ')
+    try:
+        a = int(raw_input('How many feet would you like to convert? '))
+    except ValueError:
+        print 'You need to enter a number!'
+        foot()
     b = raw_input('What you you like to convert to? [I]nches, [Y]ards, [M]iles: ')
     if b == 'I' or b == 'i':
         feet_inches(a)
