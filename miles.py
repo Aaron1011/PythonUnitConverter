0f8b3da17650a3f20450a3205131f1e45b401f18
Aaron Hill <techsavvy8989@gmail.com>
2012-03-26 20:56:32 -0400

    Added miles to yards conversion capability and added missing call to miles to feet conversion function

--- miles.py
+++ miles.py
@@ -16,6 +16,15 @@ def miles_feet(n):
         print str(n) + ' miles = ' + str(b) + ' feet.'
         quit()
 
+def miles_yards(n):
+    b = n * 1760
+    if  n == 1:
+        print str(n) + ' mile = ' + str(b) + ' yard(s).'
+        quit()
+    else:
+        print str(n) + ' miles = ' + str(b) + ' yard(s).'
+        quit() 
+
 def mile():
     try:
         a = float(raw_input('How many miles would you like to convert? '))
@@ -25,3 +34,7 @@ def mile():
     b = raw_input('What you you like to convert to? [I]nches, [F]eet, [Y]ards: ')
     if b == "I" or b == "i":
         miles_inches(a)
+    if b == 'F' or b == 'f':
+        miles_feet(a)
+    if b == 'Y' or b == 'y':
+        miles_yards(a)
