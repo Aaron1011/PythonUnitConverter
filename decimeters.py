978b97bb2af044386f98b4cc9587e4b12f0ea96e
Aaron Hill <aa1ronham@aol.com>
2012-04-19 18:57:58 -0400

    Added decimeters to millimeters conversion capability

--- decimeters.py
+++ decimeters.py
@@ -1,3 +1,13 @@
+def decimeters_millimeters(n):
+    b = float(n) * 100
+    if  float(n) == 1:
+        print str(n) + ' decimeter = ' + str(b) + ' millimeter(s).'
+        quit()
+    else:
+        print str(n) + ' decimeters = ' + str(b) + ' millimeter(s)'
+        quit()
+
+
 def decimeters_centimeters(n):
     b = float(n) * 10
     if  float(n) == 1:
@@ -13,10 +23,12 @@ def decimeter():
     except ValueError:
         print 'You need to enter a number!'
         decimeter()
-    b = raw_input('What you you like to convert to? [C]entimeters, [M]eters, [K]ilometers: ')
+    b = raw_input('What you you like to convert to? [M]illimeters, [C]entimeters, [Me]ters, [K]ilometers: ')
+    if b == 'M' or b == 'm':
+        decimeters_millimeters(a)
     if b == 'C' or b == 'c':
         decimeters_centimeters(a)
     if b == 'Y' or b == 'y':
         quit()
-    if b == 'M' or b == 'm':
+    if b == 'Me' or b == 'me':
         quit()
