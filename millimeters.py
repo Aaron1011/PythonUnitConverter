70ce2c37d116085d14c1bc17c1c266ce8417fb27
Aaron Hill <aa1ronham@aol.com>
2012-04-19 08:02:18 -0400

    Added millimeters to centimeters conversion capability

--- millimeters.py
+++ millimeters.py
@@ -1,3 +1,12 @@
+def millimeters_centimeters(n):
+    b = float(n) / 10
+    if  float(n) == 1:
+        print str(n) + ' millimeter = ' + str(b) + ' centimeter(s).'
+        quit()
+    else:
+        print str(n) + ' millimeters = ' + str(b) + ' centimeter(s).'
+        quit()
+
 def millimeter():
     try:
         a = float(raw_input('How many millimeters would you like to convert? '))
@@ -6,7 +15,7 @@ def millimeter():
         foot()
     b = raw_input('What you you like to convert to? [C]entimeters, [D]ecimeters, [M]eters, [K]ilometers: ')
     if b == 'C' or b == 'c':
-       quit()
+       millimeters_centimeters(a)
     if b == 'D' or b == 'd':
         quit()
     if b == 'M' or b == 'm':
