e1bb07f0cb6fb42efc4bf775f384b529e9fe3ab9
Aaron Hill <aa1ronham@aol.com>
2012-04-19 08:05:27 -0400

    Added millimeters to decimeters conversion capbility

--- millimeters.py
+++ millimeters.py
@@ -7,17 +7,27 @@ def millimeters_centimeters(n):
         print str(n) + ' millimeters = ' + str(b) + ' centimeter(s).'
         quit()
 
+def millimeters_decimeters(n):
+    b = float(n) / 100
+    if  float(n) == 1:
+        print str(n) + ' millimeter = ' + str(b) + ' decimeter(s).'
+        quit()
+    else:
+        print str(n) + ' millimeters = ' + str(b) + ' deciimeter(s).'
+        quit()
+
+
 def millimeter():
     try:
         a = float(raw_input('How many millimeters would you like to convert? '))
     except ValueError:
         print 'You need to enter a number!'
-        foot()
+        millimeter()
     b = raw_input('What you you like to convert to? [C]entimeters, [D]ecimeters, [M]eters, [K]ilometers: ')
     if b == 'C' or b == 'c':
        millimeters_centimeters(a)
     if b == 'D' or b == 'd':
-        quit()
+       millimeters_decimeters(a)
     if b == 'M' or b == 'm':
         quit()
     if b == 'K' or b == 'k':
