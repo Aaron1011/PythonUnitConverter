a4dc30b64ae2da75f366094d3e116d3f08465df6
Aaron Hill <aa1ronham@aol.com>
2012-04-19 19:13:25 -0400

    Added meters to millimeters conversion capability

--- meters.py
+++ meters.py
@@ -1,3 +1,12 @@
+def meters_millimeters(n):
+    b = float(n) * 1000
+    if  float(n) == 1:
+        print str(n) + ' meter = ' + str(b) + ' millimeter(s).'
+        quit()
+    else:
+        print str(n) + ' meters = ' + str(b) + ' millimeter(s)'
+        quit()
+
 def meter():
     try:
         a = float(raw_input('How many meters would you like to convert? '))
@@ -5,4 +14,7 @@ def meter():
         print 'You need to enter a number!'
         meter()
     b = raw_input('What you you like to convert to? [M]illimeters, [C]entimeters, [D]ecimeters, [K]ilometers: ')
-    quit()
+    if b == 'M' or b == 'm':
+        meters_millimeters(a)
+    else:
+        print 'That unit is not available yet!'
