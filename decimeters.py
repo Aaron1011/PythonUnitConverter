26fbb95e9d81d19aff009462944c2a315d758bd6
Aaron Hill <aa1ronham@aol.com>
2012-04-19 19:01:52 -0400

    Added decimeters to meters conversion capability

--- decimeters.py
+++ decimeters.py
@@ -17,6 +17,15 @@ def decimeters_centimeters(n):
         print str(n) + ' decimeters = ' + str(b) + ' centimeter(s)'
         quit()
 
+def decimeters_meters(n):
+    b = float(n) / 10
+    if  float(n) == 1:
+        print str(n) + ' decimeter = ' + str(b) + ' meter(s).'
+        quit()
+    else:
+        print str(n) + ' decimeters = ' + str(b) + ' meter(s)'
+        quit()
+
 def decimeter():
     try:
         a = float(raw_input('How many decimeters would you like to convert? '))
@@ -28,7 +37,7 @@ def decimeter():
         decimeters_millimeters(a)
     if b == 'C' or b == 'c':
         decimeters_centimeters(a)
-    if b == 'Y' or b == 'y':
+    if b == 'K' or b == 'k':
         quit()
     if b == 'Me' or b == 'me':
-        quit()
+        decimeters_meters(a)
