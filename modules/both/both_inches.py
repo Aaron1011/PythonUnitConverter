deea152666d687eb359d1cafc6c5f4a8b8e10ce2
Aaron Hill <aa1ronham@aol.com>
2012-05-02 08:09:39 -0400

    Added inches to centimeters conversion capability

--- modules/both/both_inches.py
+++ modules/both/both_inches.py
@@ -5,6 +5,13 @@ def inches_millimeters(n):
     else:
         print str(n) + ' inches = ' + str(a) + ' centimeters.'
 
+def inches_centimeters(n):
+    a = n * 2.54
+    if n == 1:
+        print str(n) + ' inch = ' + str(a) +  ' centimeters.'
+    else:
+        print str(n) + ' inches = ' + str(a) +  ' centimeters.'
+
 def both_inch():
     try:
         a = float(raw_input('How many inches would you like to convert? '))
@@ -14,3 +21,5 @@ def both_inch():
     b = raw_input('What would you like to convert to? [M]illimeters, [C]entimeters. [D]ecimeters, [Me]ters, [K]ilometers? ')    
     if b == 'M' or b == 'm':
         inches_millimeters(a)
+    elif b == 'C' or b == 'c':
+        inches_centimeters(a)
