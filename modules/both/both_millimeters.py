5956828ef17be0cd226bf580f73802826d6a8f78
Vuyisile Ndlovu <terrameijar@gmail.com>
2014-07-16 11:36:36 +0200

    Added Metric conversion: mm to yards and miles

--- modules/both/both_millimeters.py
+++ modules/both/both_millimeters.py
@@ -1,6 +1,6 @@
 """
-metric to english conversions to go here:
-converting millimeters to either feet, yards or miles
+Metric to English Conversions module.
+These are conversions of Millimeters to either Feet, Yards or Miles
 
 """
 def millimeters_feet(n, api=False):
@@ -12,9 +12,10 @@ def millimeters_feet(n, api=False):
 		print str(n) + ' millimeter = ' + str(a) + ' feet.'
 	else:
 		print str(n) + ' millimeters = ' + str(a) + ' feet.'
+	both_millimeter()
 
 def millimeters_yards(n, api=False):
-	#formula goes here
+	a = n * 0.0010936
 
 	if api == True:
 		return a
@@ -22,9 +23,10 @@ def millimeters_yards(n, api=False):
 		print str(n) + ' millimeter = ' + str(a) + ' yards.'
 	else:
 		print str(n) + ' millimeters = ' + str(a) + ' yards.'
+	both_millimeter()
 
-def millimeters_miles():
-	#formula goes here
+def millimeters_miles(n, api=False):
+	a = n * 0.0000006214
 
 	if api == True:
 		return a
@@ -32,6 +34,7 @@ def millimeters_miles():
 		print str(n) + ' millimeter = ' + str(a) + ' miles.'
 	else:
 		print str(n) + ' millimeters = ' + str(a) + ' miles.'
+	both_millimeter()
 
 
 def both_millimeter():
@@ -41,13 +44,16 @@ def both_millimeter():
 		print 'You need to enter a number!'
 		both_millimeter()
 
-	b = raw_input('What would you like to convert to? [F]eet, [Y]ards or [M]iles ')
+	b = raw_input('What would you like to convert to? [F]eet, [Y]ards or [M]iles [Q]uit  ')
 	if b == 'F' or b == 'f':
 		millimeters_feet(a)
 	elif b == 'Y' or b == 'y':
 		millimeters_yards(a)
 	elif b == 'M' or b == 'm':
 		millimeters_miles(a)
+	elif b == 'q' or b == 'Q':
+		print 'Goodbye \n'
+		quit()
 	else:
 		print 'That unit is not available yet'
 		both_millimeter()
